f43832883ab4a2befcaad27ac40b65911e2814b2
Adrián Vázquez <avn2000inc@gmail.com>
2025-03-18 20:11:29 -0600

    v1 Web

--- frontend/flutter_bootstrap.js
+++ frontend/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2834860117"
+    serviceWorkerVersion: "1274871070"
   }
 });
